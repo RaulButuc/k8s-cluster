2ec8d3e1f17b4de53d100ba0b2e2f9623bb19948
RaulButuc <raulbutuc@gmail.com>
2021-03-16 23:41:20 +0000

    Add a basic VPC

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.60.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file("k8s-cluster.json")
+
+  project = "k8s-cluster-7"
+  region  = "europe-west2"
+  zone    = "europe-west2-c"
+
+}
+
+resource "google_compute_subnetwork" "vpc_subnet" {
+  name          = "k8s-cluster-subnet"
+  ip_cidr_range = "10.2.0.0/16"
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "k8s-cluster-network"
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
+  mtu                     = 1500
+}
