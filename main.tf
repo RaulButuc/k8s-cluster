fd0c2bba73785740adb282ffa2e299d3c31190a0
RaulButuc <raulbutuc@gmail.com>
2021-03-17 01:11:29 +0000

    Add CentOS 7 image and VMs

--- main.tf
+++ main.tf
@@ -41,3 +41,60 @@ resource "google_compute_network" "vpc_network" {
   routing_mode            = "REGIONAL"
   mtu                     = 1500
 }
+
+data "google_compute_image" "vm_image" {
+  family  = "centos-7"
+  project = "centos-cloud"
+}
+
+resource "google_compute_instance" "master_node_1" {
+  name         = "kubemaster-001"
+  machine_type = "n1-standard-2" # 2 vCPU, 7.5 GB memory
+  zone         = "europe-west2-c"
+
+  tags = ["master", "true"]
+
+  boot_disk {
+    device_name = "master-001"
+
+    initialize_params {
+      image = data.google_compute_image.vm_image.self_link
+      size  = 20
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.vpc_subnet.id
+    
+    access_config {
+      # Auto-generate an Ephemeral IP
+    }
+  }
+}
+
+resource "google_compute_instance" "worker_node_1" {
+  name         = "kubeworker-001"
+  machine_type = "n1-standard-2" # 2 vCPU, 7.5 GB memory
+  zone         = "europe-west2-c"
+
+  boot_disk {
+    device_name = "worker-001"
+
+    initialize_params {
+      image = data.google_compute_image.vm_image.self_link
+      size  = 20
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.vpc_subnet.id
+
+    access_config {
+      # Auto-generate an Ephemeral IP
+    }
+  }
+}
